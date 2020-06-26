a9197dcb68898d2a06d938a5359d47d303ba84b2
rjawesome <aonedtop@gmail.com>
2020-06-26 16:41:29 -0700

    added basic post request

--- ALTR/static/js/dashboard.js
+++ ALTR/static/js/dashboard.js
@@ -11,6 +11,7 @@ firebase.auth().onAuthStateChanged(function (user) {
     if (user !== null) {
         email = user.email
         uid = user.uid
+        //send post request to emails and initialize all the emails
     }
     else {
         window.location.href = 'login.html'
@@ -20,4 +21,33 @@ firebase.auth().onAuthStateChanged(function (user) {
 
 document.getElementById("logout-btn").onclick = () => {
     firebase.auth().signOut()
+}
+
+createEmail = (name) => {
+    if (uid === undefined || email === undefined) return;
+    jQuery.ajax({
+        url: 'http://altr.cf/create',
+        type: 'POST',
+        data: JSON.stringify({
+          uid: uid,
+          youremail: email,
+          name: name
+        }),
+        contentType: 'application/json',
+        beforeSend: function(x) {
+          if (x && x.overrideMimeType) {
+            x.overrideMimeType('application/json;charset=UTF-8')
+          }
+        },
+        success: function(result) {
+          var res = result.res;
+          if (res === 0) {
+            var email = result.email
+            //create an emlement for that email
+          }
+          else {
+              console.log(res);
+          }
+        },
+    })
 }
