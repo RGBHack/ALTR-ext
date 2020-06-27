4a815bdd7374520babfdd33899f0cd0ef278d786
rjawesome <aonedtop@gmail.com>
2020-06-27 16:09:11 -0700

    prettier and start functions

--- ALTR/static/js/dashboard.js
+++ ALTR/static/js/dashboard.js
@@ -7,15 +7,15 @@ if (firebase.apps.length === 0) {
     firebase.initializeApp(config)
 }
 
-firebase.auth().onAuthStateChanged(function (user) {
+firebase.auth().onAuthStateChanged(function(user) {
     if (user !== null) {
         email = user.email
         uid = user.uid
-        //send post request to emails and initialize all the emails
-    }
-    else {
+        console.log(email + " " + uid)
+            //send post request to emails and initialize all the emails
+    } else {
         window.location.href = 'login.html'
-        chrome.browserAction.setPopup({popup: "login.html"});
+        chrome.browserAction.setPopup({ popup: "login.html" });
     }
 })
 
@@ -23,31 +23,172 @@ document.getElementById("logout-btn").onclick = () => {
     firebase.auth().signOut()
 }
 
+
+
 createEmail = (name) => {
     if (uid === undefined || email === undefined) return;
     jQuery.ajax({
         url: 'http://altr.cf/create',
         type: 'POST',
         data: JSON.stringify({
-          uid: uid,
-          youremail: email,
-          name: name
+            uid: uid,
+            youremail: email,
+            username: name
         }),
         contentType: 'application/json',
         beforeSend: function(x) {
-          if (x && x.overrideMimeType) {
-            x.overrideMimeType('application/json;charset=UTF-8')
-          }
+            if (x && x.overrideMimeType) {
+                x.overrideMimeType('application/json;charset=UTF-8')
+            }
         },
         success: function(result) {
-          var res = result.res;
-          if (res === 0) {
-            var email = result.email
-            //create an emlement for that email
-          }
-          else {
-              console.log(res);
-          }
+            var res = result.res;
+            if (res === 0) {
+                var email = result.email
+                console.log(email)
+            } else {
+                console.log(res);
+            }
+        },
+    })
+}
+
+onEmail = (name) => {
+    if (uid === undefined || email === undefined) return;
+    jQuery.ajax({
+        url: 'http://altr.cf/on',
+        type: 'POST',
+        data: JSON.stringify({
+            youremail: email,
+            email: uid,
+            uid: name
+        }),
+        contentType: 'application/json',
+        beforeSend: function(x) {
+            if (x && x.overrideMimeType) {
+                x.overrideMimeType('application/json;charset=UTF-8')
+            }
+        },
+        success: function(result) {
+            var res = result.res;
+            if (res === 0) {
+                var email = result.email
+                    //create an emlement for that email
+            } else {
+                console.log(res);
+            }
+        },
+    })
+}
+
+offEmail = (name) => {
+    if (uid === undefined || email === undefined) return;
+    jQuery.ajax({
+        url: 'http://altr.cf/create',
+        type: 'POST',
+        data: JSON.stringify({
+            uid: uid,
+            youremail: email,
+            name: name
+        }),
+        contentType: 'application/json',
+        beforeSend: function(x) {
+            if (x && x.overrideMimeType) {
+                x.overrideMimeType('application/json;charset=UTF-8')
+            }
+        },
+        success: function(result) {
+            var res = result.res;
+            if (res === 0) {
+                var email = result.email
+                    //create an emlement for that email
+            } else {
+                console.log(res);
+            }
+        },
+    })
+}
+
+status = (name) => {
+    if (uid === undefined || email === undefined) return;
+    jQuery.ajax({
+        url: 'http://altr.cf/create',
+        type: 'POST',
+        data: JSON.stringify({
+            uid: uid,
+            youremail: email,
+            name: name
+        }),
+        contentType: 'application/json',
+        beforeSend: function(x) {
+            if (x && x.overrideMimeType) {
+                x.overrideMimeType('application/json;charset=UTF-8')
+            }
+        },
+        success: function(result) {
+            var res = result.res;
+            if (res === 0) {
+                var email = result.email
+                    //create an emlement for that email
+            } else {
+                console.log(res);
+            }
+        },
+    })
+}
+
+allAliases = (name) => {
+    if (uid === undefined || email === undefined) return;
+    jQuery.ajax({
+        url: 'http://altr.cf/create',
+        type: 'POST',
+        data: JSON.stringify({
+            uid: uid,
+            youremail: email,
+            name: name
+        }),
+        contentType: 'application/json',
+        beforeSend: function(x) {
+            if (x && x.overrideMimeType) {
+                x.overrideMimeType('application/json;charset=UTF-8')
+            }
+        },
+        success: function(result) {
+            var res = result.res;
+            if (res === 0) {
+                var email = result.email
+                    //create an emlement for that email
+            } else {
+                console.log(res);
+            }
+        },
+    })
+}
+
+deleteAlias = (name) => {
+    if (uid === undefined || email === undefined) return;
+    jQuery.ajax({
+        url: 'http://altr.cf/create',
+        type: 'POST',
+        data: JSON.stringify({
+            uid: uid,
+            youremail: email,
+            name: name
+        }),
+        contentType: 'application/json',
+        beforeSend: function(x) {
+            if (x && x.overrideMimeType) {
+                x.overrideMimeType('application/json;charset=UTF-8')
+            }
+        },
+        success: function(result) {
+            var res = result.res;
+            if (res === 0) {
+                var email = result.email
+                    //create an emlement for that email
+            } else {
+                console.log(res);
+            }
         },
     })
 }
